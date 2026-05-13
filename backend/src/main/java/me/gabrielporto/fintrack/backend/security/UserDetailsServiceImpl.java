61e57b75f06d49720281c71d4049d258d20f6050
Gabriel Porto <gpalmenara@gmail.com>
2026-05-13 20:30:37 -0300

    feat: adição de config de JWT, CORS, Auth

---
+++ backend/src/main/java/me/gabrielporto/fintrack/backend/security/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package me.gabrielporto.fintrack.backend.security;
+
+import lombok.RequiredArgsConstructor;
+import me.gabrielporto.fintrack.backend.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .map(user -> org.springframework.security.core.userdetails.User
+                        .withUsername(user.getEmail())
+                        .password(user.getPassword())
+                        .build())
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado: " + email));
+    }
+}
